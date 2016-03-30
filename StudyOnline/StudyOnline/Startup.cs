6406e10053881fd1e83b1ed089c42f6d6acbee18
NamCkiku <tranhoangnam11373@gmail.com>
2016-03-30 11:37:23 +0700

    add project

---
+++ StudyOnline/StudyOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudyOnline.Startup))]
+namespace StudyOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
